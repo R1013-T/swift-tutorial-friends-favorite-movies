54ba5bdcce074d1c547bd3753310457dbf3b6ee3
R1013-T <takahashi.ryunosuke.job@gmail.com>
2025-01-21 01:36:40 +0900

    🚀

---
+++ FriendsFavoriteMovies/MovieDetail.swift
@@ -0,0 +1,17 @@
+//
+//  MovieDetail.swift
+//  FriendsFavoriteMovies
+//
+    
+
+import SwiftUI
+
+struct MovieDetail: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MovieDetail()
+}
